2f9a37fff2a970f6257ac4725f904619de2db3c1
Luis Fernando Martinez Sanhueza <l.martinez07@ufromail.cl>
2021-01-09 15:52:13 -0300

    agregue parte Ricardo

--- app1/views.py
+++ app1/views.py
@@ -3,3 +3,39 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+
+def inicio_sesion(request):
+    return render(request,"inicio_sesion.html")
+
+def registro(request):
+    return render(request,"registro.html")
+
+def home(request):
+    return render(request,"home.html")
+
+def guardado(request):
+    return render(request,"guardado.html")
+
+def busqueda(request):
+    return render(request,"busqueda.html")
+
+def resultado_busqueda(request):
+    return render(request,"resultado_busqueda.html")
+
+def publicar(request):
+    return render(request,"publicar.html") 
+
+def editar(request):
+    return render(request,"editar.html")
+
+def publicacion(request):
+    return render(request,"publicacion.html")
+
+def perfil(request):
+    return render(request,"perfil.html")
+
+def miperfl(request):
+    return render(request,"miperfil.html")
+
+def editar_perfil(request):
+    return render(request,"editar_perfil.html")
