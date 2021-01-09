a108ad9d6f6a0df5a88642b8231f4f7fc93fd96c
Ricardo Millanao Burgos <r.millanao02@ufromail.cl>
2021-01-09 14:56:39 -0300

    creando urls

--- app1/urls.py
+++ app1/urls.py
@@ -2,5 +2,17 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.index),
+    path('nashcode.com',views.index),
+    path('nashcode.com/iniciosesion',views.inicio_sesion),
+    path('nashcode.com/registro',views.registro),
+    path('nashcode.com/<string : username>/home',views.home),
+    path('nashcode.com/<string : username>/guardado',views.guardado),
+    path('nashcode.com/<string : username>/busqueda',views.busqueda),
+    path('nashcode.com/<string : username>/resultadobusqueda',views.resultado_busqueda),
+    path('nashcode.com/<string : username>/publicar',views.publicar),
+    path('nashcode.com/<string : username>/editar/<int : id>',views.editar),
+    path('nashcode.com/<string : username>/publicacion/<int : id>',views.publicacion),
+    path('nashcode.com/<string : username>/perfil',views.perfil),
+    path('nashcode.com/<string : username>/miperfil',views.mi_perfil),
+    path('nashcode.com/<string : username>/editarperfil',views.editar_perfil),
 ]
