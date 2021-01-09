5770e74d600821d1cde5333aa4a300c1404ac70c
Luis Fernando Martinez Sanhueza <l.martinez07@ufromail.cl>
2021-01-09 17:51:10 -0300

    models casi listos

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key = True)
+    alias =  models.CharField(max_length = 20)
+    nombre =  models.CharField(max_length = 60)
+    email =  models.CharField(max_length = 60)
+    password =  models.CharField(max_length = 10)
+    likes = models.IntegerField()
+    fecha_creacion = models.DateTimeField(auto_now_add = True)
+    fecha_modificacion = models.DateTimeField(auto_now = True)
+
+class Publicacion(models.Model):
+    id_publicacion = models.AutoField(primary_key = True)
+    usuario = models.ForeignKey(Usuario, null = True, blank = True, on_delete = models.CASCADE)
+    titulo = models.CharField(max_length = 30)
+    codigo = models.TextField()
+    descripcion = models.TextField()
+    categoria = models.CharField(max_length = 10)
+    likes = models.IntegerField()
+    fecha_creacion = models.DateTimeField(auto_now_add = True)
+    fecha_modificacion = models.DateTimeField(auto_now = True)
